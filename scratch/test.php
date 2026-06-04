828be8b050cf16caa4663c711d5f2313215f1d24
Admin <admin@source.test>
2026-06-04 07:23:46 +0700

    Update test scratch scripts and log outputs

--- scratch/test.php
+++ scratch/test.php
@@ -1,5 +1,5 @@
 <?php
 define('DEBUG_ROUTE', true);
-$_SERVER['REQUEST_URI'] = '/san-pham/test-san-pham-bien-the';
+$_SERVER['REQUEST_URI'] = '/en/';
 $_SERVER['REQUEST_METHOD'] = 'GET';
 require __DIR__ . '/../index.php';
